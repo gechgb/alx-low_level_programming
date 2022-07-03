85bd1cf9fbc49ff51be753aad2bb968c60bed4ec
gechgb <gechgberhe@gmail.com>
2022-07-03 19:48:01 +0300

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cat;
+	cat = 97;
+	while (cat<123)
+	{
+		putchar(cat);
+		cat++;
+	}
+	putchar('\n');
+	return (0);
+}
