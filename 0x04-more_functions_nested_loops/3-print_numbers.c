fe510534b3639b19fd7f65b2c2460cc0374d368e
gechgb <gechgberhe@gmail.com>
2022-07-08 18:55:37 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - multiplies two integers
+*
+* Return: return 0
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
+
