d239e9a497034987445a5316ab305935087c9368
gechgb <gechgberhe@gmail.com>
2022-07-11 19:14:10 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* swap_int - swap two int
+* @a: pointer one
+* @b: pointer two
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+	while (*(a + s))
+			{
+			a++;
+			}
+		return (a);
+}
