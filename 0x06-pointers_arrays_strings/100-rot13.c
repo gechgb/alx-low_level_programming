30c095383e9413f64c1c81799d507ac6ecddd2a0
gechgb <gechgberhe@gmail.com>
2022-07-15 22:56:08 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,9 +2,8 @@
 /**
  * rot13 - encodes a string using rot13
  * @s: input string.
- * Return: 0
+ * Return: s
  */
-
 char *rot13(char *s)
 {
 	int a = 0;
@@ -12,17 +11,17 @@ char *rot13(char *s)
 	char al[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char en[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-   while (*(s + a) != '\0')
-   {
-   		for (b = 0; b < 52; b++)
-   		{
-   			if (*(s + a) == al[a])
-   			{
-   				*(s + a) = en[a];
-   				break;
-			   }
-		   }
-		   a++;
+	while (*(s + a) != '\0')
+	{
+	for (b = 0; b < 52; b++)
+	{
+		if (*(s + a) == al[a])
+		{
+			*(s + a) = en[a];
+			break;
+		}
+	}
+	a++;
 	}
 	return (s);
 }
