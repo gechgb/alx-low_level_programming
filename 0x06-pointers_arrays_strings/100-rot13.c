7c60ba440973c64c7868f50ff0e04af176958585
gechgb <gechgberhe@gmail.com>
2022-07-15 22:43:35 +0300

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: 0
+ */
+
+char *rot13(char *s)
+{
+	int a = 0;
+	int b;
+	char al[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char en[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+   while (*(s + a) != '\0')
+   {
+   		for (b = 0; b < 52; b++)
+   		{
+   			if (*(s + a) == al[a])
+   			{
+   				*(s + a) = en[a];
+   				break;
+			   }
+		   }
+		   a++;
+	}
+	return (s);
+}
