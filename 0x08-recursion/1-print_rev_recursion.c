a79c5ce129fd5a919101b4f0afd47de53adf1f91
gechgb <gechgberhe@gmail.com>
2022-07-20 19:16:35 +0300

    0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _print_rev_recursion - a function that prints a string in reverse.
+ * @s: pointer
+**/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
