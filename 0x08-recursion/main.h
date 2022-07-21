9cffa596adcbb6ca41a7b63fefa746db45811928
gechgb <gechgberhe@gmail.com>
2022-07-21 23:14:36 +0300

    0x08-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2)
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
