29985c8f124876cf7b0724a30cb13a6de153ebba
gechgb <gechgberhe@gmail.com>
2022-07-23 19:41:45 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print argc
+ *@argc: first argument
+ *@argv: arry
+ *Return: (0)
+**/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
