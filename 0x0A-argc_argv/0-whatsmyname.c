fc38e18660c46d58fe38a790e6e894afde721778
gechgb <gechgberhe@gmail.com>
2022-07-23 19:24:59 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stlib>
+/**
+ * main - print argv
+ *@argc: first argument
+ *@argv: arry
+ *Return: (0)
+**/
+int main(int argc, *char argv[])
+{
+	printf("%d\n", argv[0]);
+	return (0);
+}
